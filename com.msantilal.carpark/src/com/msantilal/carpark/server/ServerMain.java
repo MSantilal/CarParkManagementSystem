09765fd1e35970bd2302b4104d050fd55d199144
MSantilal <MSantilal@users.noreply.github.com>
2015-11-05 19:13:09 +0000

    Changes in folder structure

---
+++ com.msantilal.carpark/src/com/msantilal/carpark/server/ServerMain.java
@@ -0,0 +1,8 @@
+package com.msantilal.carpark.server;
+
+/**
+ * Created by Monil on 05/11/2015.
+ */
+public class ServerMain
+{
+}
--- com.msantilal.carpark/src/server/CarParkMain.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-/**
- * Created by Monil on 05/11/2015.
- */
-public class CarParkMain
-{
-}
